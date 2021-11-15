8edf9b149ae8e0893966e302ce415cff6a03dcbc
StefanHagewoud <stefanhagewoud@gmail.com>
2021-11-15 15:44:57 +0100

    Dingen

--- Assets/Kmar Project/Stefan/MainMenu/MenuScript.cs
+++ Assets/Kmar Project/Stefan/MainMenu/MenuScript.cs
@@ -2,16 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class MenuScript : MonoBehaviour
 {
     // Start is called before the first frame update
     // public Scene level1;
-    public GameObject mainMenu;
-    public GameObject settingsMenu;
-    public GameObject levelSelectMenu;
-
 
     void Start()
     {
@@ -22,34 +17,6 @@ public class MenuScript : MonoBehaviour
     void Update()
     {
         
-    }
-
-    public void ButtonPressed(Button button)
-    {
-        if (button.gameObject.name == "Settings")
-        {
-            waiter(1);
-            mainMenu.SetActive(false);
-            settingsMenu.SetActive(true);
-            levelSelectMenu.SetActive(false);
-        }
-        else if (button.gameObject.name == "Back")
-        {
-            mainMenu.SetActive(true);
-            settingsMenu.SetActive(false);
-            levelSelectMenu.SetActive(false);
-        }
-        else if (button.gameObject.name == "Play")
-        {
-            mainMenu.SetActive(false);
-            settingsMenu.SetActive(false);
-            levelSelectMenu.SetActive(true);
-        }
-    }
-
-    public IEnumerator waiter(int seconds)
-    {
-        yield return new WaitForSecondsRealtime(seconds);
     }
 
     public void StartLevel()
