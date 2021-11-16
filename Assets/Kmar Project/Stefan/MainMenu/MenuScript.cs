576d0a25c96ceea2318ee22fc397f1c1cb793783
StefanHagewoud <stefanhagewoud@gmail.com>
2021-11-16 15:54:59 +0100

    UI werkt deels

--- Assets/Kmar Project/Stefan/MainMenu/MenuScript.cs
+++ Assets/Kmar Project/Stefan/MainMenu/MenuScript.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuScript : MonoBehaviour
 {
     // Start is called before the first frame update
     // public Scene level1;
+    public GameObject mainMenu;
+    public GameObject settingsMenu;
+    public GameObject levelSelectMenu;
+
 
     void Start()
     {
@@ -17,6 +22,28 @@ public class MenuScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void ButtonPressed(Button button)
+    {
+        if (button.gameObject.name == "Settings")
+        {
+            mainMenu.SetActive(false);
+            settingsMenu.SetActive(true);
+            levelSelectMenu.SetActive(false);
+        }
+        else if (button.gameObject.name == "Back")
+        {
+            mainMenu.SetActive(true);
+            settingsMenu.SetActive(false);
+            levelSelectMenu.SetActive(false);
+        }
+        else if (button.gameObject.name == "Play")
+        {
+            mainMenu.SetActive(false);
+            settingsMenu.SetActive(false);
+            levelSelectMenu.SetActive(true);
+        }
     }
 
     public void StartLevel()
