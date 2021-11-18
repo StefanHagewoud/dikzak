f7a818d5e00c9e08a77b87d8cfc0909d498dba9f
StefanHagewoud <stefanhagewoud@gmail.com>
2021-11-18 16:45:21 +0100

    Post processing + level list kanker

--- Assets/Kmar Project/Stefan/ButtonAnimations/TransitionScript.cs
+++ Assets/Kmar Project/Stefan/ButtonAnimations/TransitionScript.cs
@@ -5,13 +5,9 @@ using UnityEngine.UI;
 
 public class TransitionScript : MonoBehaviour
 {
-    // public GameObject otherCanvas;
     // Start is called before the first frame update
     // public AnimationClip clip;
     public AnimationClip clip;
-    public GameObject mainMenu;
-    public GameObject settingsMenu;
-    public GameObject levelSelectMenu;
     void Start()
     {
         
@@ -28,34 +24,5 @@ public class TransitionScript : MonoBehaviour
         var animation = GetComponent<Animation>();
         animation.clip = clip;
         animation.Play();
-    }
-
-    public void Start2(Button button)
-    {
-        Transition();
-        StartCoroutine(setActive(button));
-    }
-
-    IEnumerator setActive(Button button)
-    {
-        yield return new WaitForSecondsRealtime(0.5f);
-        if (button.gameObject.name == "Settings")
-        {
-            mainMenu.SetActive(false);
-            settingsMenu.SetActive(true);
-            levelSelectMenu.SetActive(false);
-        }
-        else if (button.gameObject.name == "Back")
-        {
-            mainMenu.SetActive(true);
-            settingsMenu.SetActive(false);
-            levelSelectMenu.SetActive(false);
-        }
-        else if (button.gameObject.name == "Play")
-        {
-            mainMenu.SetActive(false);
-            settingsMenu.SetActive(false);
-            levelSelectMenu.SetActive(true);
-        }
     }
 }
