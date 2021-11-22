ee3d21a3c715b76eaad741cad9e450e17095fdbc
JosRigterink <rigterinkjos@gmail.com>
2021-11-22 17:03:05 +0100

    enemyChanges

---
+++ Assets/Kmar Project/Jos/DetectScript.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectScript : MonoBehaviour
+{
+    bool detected;
+    GameObject target;
+    public Transform enemy;
+
+    public GameObject bullet;
+    public Transform shootPoint;
+
+    public float shootSpeed = 10f;
+    public float timeToShoot = 1.3f;
+    float originalTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalTime = timeToShoot;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (detected)
+        {
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition;
+            GetComponent<PatrollScript>().enabled = false;
+            //enemy.LookAt(target.transform);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (detected)
+        {
+            timeToShoot -= Time.deltaTime;
+
+            if (timeToShoot < 0)
+            {
+                ShootPlayer();
+                timeToShoot = originalTime;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            detected = true;
+            target = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            detected = false;
+            GetComponent<PatrollScript>().enabled = true;
+        }
+    }
+  
+    private void ShootPlayer()
+    {
+        GameObject currentBullet = Instantiate(bullet, shootPoint.position, shootPoint.rotation);
+        Rigidbody rig = currentBullet.GetComponent<Rigidbody>();
+
+        rig.AddForce(transform.forward * shootSpeed, ForceMode.VelocityChange);
+    }
+}
