a19f18eebe533e7104976c6ee8c71f90e8e1ff0e
JosRigterink <rigterinkjos@gmail.com>
2021-11-22 10:24:05 +0100

    enemy changes

---
+++ Assets/Kmar Project/Jos/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float horizontal;
+    public float vertical;
+    public Vector3 move;
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+
+        move.x = horizontal;
+        move.z = vertical;
+
+        GetComponent<Transform>().Translate(move * Time.deltaTime * speed);
+    }
+}
