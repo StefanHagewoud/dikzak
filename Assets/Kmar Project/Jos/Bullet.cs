98a1beaa278e25303b8b8c471cd078ec73ab8d3b
JosRigterink <rigterinkjos@gmail.com>
2021-11-26 16:44:09 +0100

    EnemyDamageChanges

---
+++ Assets/Kmar Project/Jos/Bullet.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float damage = 50;
+    public float speed = 20f;
+    public float time = 2f;
+    private float lifetimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lifetimer = time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+
+        lifetimer -= Time.deltaTime;
+        if (lifetimer <= 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("trigger!");
+            other.transform.GetComponent<PlayerScript>().TakeDamage(damage = 50); 
+        }
+    }
+}
