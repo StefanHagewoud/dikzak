0795b002e372da3ad489551aea46692ea69ed793
NKaap <NKaap@users.noreply.github.com>
2021-12-14 00:40:06 +0100

    LockPuzzleUpdate werkt nog niet heleemaal

--- Assets/Kmar Project/Noah/Sleutels/Key.cs
+++ Assets/Kmar Project/Noah/Sleutels/Key.cs
@@ -1,34 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Key : MonoBehaviour
 {
+    [Header("Key Settings")]
     public string keyNumber;
     public Text keyText;
+    public bool correctKey;
+    public bool isFilled;
 
     Color32[] colors = new Color32[4];
     private int randomColor;
     public string selectedColor;
+
+    [Header("Lock Settings")]
+    public Sprite currentImage;
+    public string currentSymbol;
+
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
+
         keyText = GetComponentInChildren<Text>();
 
-        keyNumber = Random.Range(0, 9999).ToString();
-        keyText.text = keyNumber;
-
-        /*
-        //Set Random Color
-        Color randomColor = new Color(
-            Random.Range(0f, 1f), //Red
-            Random.Range(0f, 1f), //Green
-            Random.Range(0f, 1f), //Blue
-            1);  //Transparency
-
-        this.gameObject.GetComponent<Image>().color = randomColor;
-        */
 
         //Get Random Color out of this list
         colors[0] = new Color32(0, 88, 168, 255); //Blue
@@ -36,13 +33,35 @@ public class Key : MonoBehaviour
         colors[2] = new Color32(168, 0, 75, 255); //Pink
         colors[3] = new Color32(168, 141, 0, 255); //Yellow
 
+        //Gives Object Random Color an Number
+        RandomSettings();
+
+        //Set Color into String
+        ColorToString();
+
+    }
+    public void Update()
+    {
+           
+    }
+
+    public void RandomSettings()
+    {
+        keyNumber = Random.Range(0, 999).ToString();
+        keyText.text = keyNumber;
+
         Debug.Log("Gettin random color");
         randomColor = Random.Range(0, colors.Length);
         this.gameObject.GetComponent<Image>().color = colors[randomColor];
 
-        //Set Color into String
-        ColorToString();
+        if (isFilled == false)
+        {
+            Debug.Log("gello");
+            CheckCorrectKey();
+            isFilled = true;
+        }
     }
+
     public void ColorToString()
     {
         if (randomColor == 0)
@@ -60,6 +79,121 @@ public class Key : MonoBehaviour
         if (randomColor == 3)
         {
             selectedColor = "Yellow";
+        }
+    }
+    public void CheckCorKey()
+    {
+        if(currentImage.name == "CircleLock")
+        {
+
+        }
+        if (currentImage.name == "HearthLock")
+        {
+
+        }
+        if (currentImage.name == "SquareLock")
+        {
+
+        }
+        if (currentImage.name == "TriangleLock")
+        {
+
+        }
+        if (selectedColor == "Blue")
+        {
+
+        }
+        if (selectedColor == "Green")
+        {
+
+        }
+        if (selectedColor == "Pink")
+        {
+
+        }
+        if (selectedColor == "Yellow")
+        {
+
+        }
+    }
+    public void CheckCorrectKey()
+    {
+        Debug.Log(int.Parse(keyNumber));
+        if(currentImage.name == "CircleLock" && selectedColor == "Blue")
+        {
+            if(currentSymbol == "@" && Enumerable.Range(1, 250).Contains(int.Parse(keyNumber)))
+            {
+                correctKey = true;
+            }
+            if (currentSymbol == "#" && Enumerable.Range(251, 500).Contains(int.Parse(keyNumber)))
+            {
+                correctKey = true;
+            }
+            if (currentSymbol == "$" && Enumerable.Range(501, 750).Contains(int.Parse(keyNumber)))
+            {
+                correctKey = true;
+            }
+            if (currentSymbol == "%" && Enumerable.Range(751, 1000).Contains(int.Parse(keyNumber)))
+            {
+                correctKey = true;
+            }
+        }
+        if (currentImage.name == "HearthLock" && selectedColor == "Green")
+        {
+            if (currentSymbol == "@" && Enumerable.Range(1, 250).Contains(int.Parse(keyNumber)))
+            {
+                correctKey = true;
+            }
+            if (currentSymbol == "#" && Enumerable.Range(251, 500).Contains(int.Parse(keyNumber)))
+            {
+                correctKey = true;
+            }
+            if (currentSymbol == "$" && Enumerable.Range(501, 750).Contains(int.Parse(keyNumber)))
+            {
+                correctKey = true;
+            }
+            if (currentSymbol == "%" && Enumerable.Range(751, 1000).Contains(int.Parse(keyNumber)))
+            {
+                correctKey = true;
+            }
+        }
+        if (currentImage.name == "SquareLock" && selectedColor == "Pink")
+        {
+            if (currentSymbol == "@" && Enumerable.Range(1, 250).Contains(int.Parse(keyNumber)))
+            {
+                correctKey = true;
+            }
+            if (currentSymbol == "#" && Enumerable.Range(251, 500).Contains(int.Parse(keyNumber)))
+            {
+                correctKey = true;
+            }
+            if (currentSymbol == "$" && Enumerable.Range(501, 750).Contains(int.Parse(keyNumber)))
+            {
+                correctKey = true;
+            }
+            if (currentSymbol == "%" && Enumerable.Range(751, 1000).Contains(int.Parse(keyNumber)))
+            {
+                correctKey = true;
+            }
+        }
+        if (currentImage.name == "TriangleLock" && selectedColor == "Yellow")
+        {
+            if (currentSymbol == "@" && Enumerable.Range(1, 250).Contains(int.Parse(keyNumber)))
+            {
+                correctKey = true;
+            }
+            if (currentSymbol == "#" && Enumerable.Range(251, 500).Contains(int.Parse(keyNumber)))
+            {
+                correctKey = true;
+            }
+            if (currentSymbol == "$" && Enumerable.Range(501, 750).Contains(int.Parse(keyNumber)))
+            {
+                correctKey = true;
+            }
+            if (currentSymbol == "%" && Enumerable.Range(751, 1000).Contains(int.Parse(keyNumber)))
+            {
+                correctKey = true;
+            }
         }
     }
 }
--- Assets/Kmar Project/Noah/Sleutels/KeysController.cs
+++ Assets/Kmar Project/Noah/Sleutels/KeysController.cs
@@ -5,16 +5,85 @@ using UnityEngine.UI;
 
 public class KeysController : MonoBehaviour
 {
+    [Header("Key Settings")]
     public List<GameObject> keys;
+
+    [Header("Lock Settings")]
+    public Text lockText;
+    public Image lockImg;
+    public Sprite currentLockSprite;
+    public Sprite[] lockSprites;
+    private int randomSprite;
+    public string[] lockTextSym;
+    private int randomText;
+
+    public int incorrectKeys;
+    private int checkHelper;
+
+
     // Start is called before the first frame update
     void Start()
     {
         keys.AddRange(GameObject.FindGameObjectsWithTag("Keys"));
+
+        randomSprite = Random.Range(0, lockSprites.Length);
+        lockImg.GetComponent<Image>().sprite = lockSprites[randomSprite];
+        currentLockSprite  = lockSprites[randomSprite];
+
+        randomText = Random.Range(0, lockTextSym.Length);
+        lockText.GetComponent<Text>().text = lockTextSym[randomText];
+
+        for(int i = 0; 1<= keys.Count; i++)
+        {
+            keys[i].GetComponent<Key>().currentImage = currentLockSprite;
+            keys[i].GetComponent<Key>().currentSymbol = lockText.text;
+            if(i == keys.Count)
+            {
+                KeyManagerHelper();
+            }
+        }
     }
+    // Sprite 0 = Circle
+    // Sprite 1  = Hearth
+    // Sprite 2 = Square
+    // Sprite 3 = Triangle
 
     // Update is called once per frame
+
+    public void KeyManagerHelper()
+    {
+        incorrectKeys = 0;
+        for (int i = 0; i < keys.Count; i++)
+        {
+            if (keys[i].GetComponent<Key>().correctKey == false)
+            {
+                incorrectKeys++;
+                Debug.Log("Incorrect Keys Plus One");
+            }
+        }
+    }
     void Update()
     {
-        
+        if (incorrectKeys >= 8)
+        {
+            if(incorrectKeys <= 7)
+            {
+                return;
+            }
+            else
+            {
+                for (checkHelper = 0; checkHelper < keys.Count; checkHelper++)
+                {
+                    keys[checkHelper].GetComponent<Key>().RandomSettings();
+                    KeyManagerHelper();
+                }
+            }
+        }
+        else
+        {
+            incorrectKeys = 0;
+            return;
+        }
+
     }
 }
