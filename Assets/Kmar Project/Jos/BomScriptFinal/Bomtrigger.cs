fbba3bc45bc54d905d7b519fbd77d8e614f392ab
JosRigterink <rigterinkjos@gmail.com>
2022-01-21 16:46:33 +0100

    trigger changes

---
+++ Assets/Kmar Project/Jos/BomScriptFinal/Bomtrigger.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bomtrigger : MonoBehaviour
+{
+    public GameObject bom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "player")
+        {
+            bom.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
